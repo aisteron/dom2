26d0c87ff40f195e4d4be135d0c1092443e27c95
aisteron <timotheus@list.ru>
2016-09-02 15:46:30 +0300

    вроде все..

---
+++ send.php
@@ -0,0 +1,22 @@
+﻿<?php
+//echo $_POST['name'].$_POST['phone'];
+
+
+
+$to = 'timotheus@list.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Заголовок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+					<h3>Форма обратной связи на сайте whg.su:</h3>
+                        <p><b>Имя: </b>'.$_POST['name'].'</p>
+						<p><b>Телефон: </b>'.$_POST['phone'].'</p>                      
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: whg.su <robot@whg.su>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+		echo '<p>Данные успешно отправлены. Ожидайте звонка менеджера</p>';
